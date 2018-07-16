3d66e0c6554c0a38c538c3ea10a43e17c3df67bd
Jeremy LUSINE <jelusine@e2r10p16.42.fr>
2018-07-16 02:39:12 +0200

    because this one suck

---
+++ Get_next_life/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jelusine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/10 15:11:45 by jelusine          #+#    #+#             */
+/*   Updated: 2018/06/23 22:51:18 by jelusine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "./libft/libft.h"
+
+# define BUFF_SIZE 8
+
+int		get_next_line(const int fd, char **line);
+
+#endif
