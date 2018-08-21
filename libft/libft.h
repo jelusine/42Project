90d9ed596820d0ef1934c3ea3f24b17933821335
Jeremy LUSINE <jelusine@e3r5p4.42.fr>
2018-08-21 03:13:49 +0200

    ft_swap

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jelusine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/03 18:18:28 by jelusine          #+#    #+#             */
-/*   Updated: 2018/08/21 00:09:00 by jelusine         ###   ########.fr       */
+/*   Updated: 2018/08/21 03:13:18 by jelusine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,6 +116,7 @@ int				ft_analyse_base(const char *str);
 int				ft_atoi_base(const char *str, const char *base);
 int				get_next_line(int fd, char **line);
 int				ft_errormsg(char const *str);
+int				ft_swap(int *a, int *b);
 char			*ft_itoa_base(int nb, int base);
 char			**ft_split(const char *str, char *kwords);
 void			ft_putnbr_base(int nbr, const char *base);
